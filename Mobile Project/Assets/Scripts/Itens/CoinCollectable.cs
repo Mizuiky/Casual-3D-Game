7becefd470d38d9231d923af42b25cc1c5f536c1
Priscila da Rocha Alves <priscila.mizuiky@gmail.com>
2022-07-28 10:31:35 -0300

    Change coin prefab to do lerp when player is getting coins

--- Mobile Project/Assets/Scripts/Itens/CoinCollectable.cs
+++ Mobile Project/Assets/Scripts/Itens/CoinCollectable.cs
@@ -4,13 +4,44 @@ using UnityEngine;
 
 public class CoinCollectable : ItemCollectableBase
 {
+    [Header("Coin Fields")]
+
+    [SerializeField]
+    private float _lerpDuration;
+
+    [SerializeField]
+    private float _speedModifier;
+
+    [SerializeField]
+    private float _minDistance;
+
+    private bool _colleted;
+
     public override void Collect()
     {
-        base.Collect();
+        _colleted = true;
+        OnCollect();
     }
 
     public override void OnCollect()
     {
         base.OnCollect();
+    }
+
+    public override void HideItem()
+    {
+        _colleted = false;
+        base.HideItem();
+    }
+
+    private void Update()
+    {
+        if (_colleted)
+        {
+            transform.position = Vector3.Lerp(transform.position, _player.transform.position, _lerpDuration * Time.deltaTime * _speedModifier);
+
+            if (Vector3.Distance(transform.position, _player.transform.position) < _minDistance)
+                base.HideItem();
+        }
     }
 }
