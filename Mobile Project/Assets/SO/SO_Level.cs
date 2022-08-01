e500dbb08b85aa8958ad5ec53c06702e7da1561a
Priscila da Rocha Alves <priscila.mizuiky@gmail.com>
2022-08-01 16:53:41 -0300

    Add scriptable object to handle level setup

---
+++ Mobile Project/Assets/SO/SO_Level.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu]
+public class SO_Level : ScriptableObject
+{
+    [Header("Dinamic Level Setup ")]
+
+    public ArtType _type;
+
+    [Header("Pieces")]
+
+    public LevelPiece _startLevelPiece;
+
+    public List<LevelPiece> _levelPieces;
+
+    public LevelPiece _endLevelPiece;
+
+    public float _timeBetweenPieces;
+
+    public int _numberOfPieces;
+}
