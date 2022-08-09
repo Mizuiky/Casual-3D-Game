532bd0287c9cf7ebac304d14d387e909ec47ec5b
Priscila da Rocha Alves <priscila.mizuiky@gmail.com>
2022-08-09 16:13:05 -0300

    Prefabs change to add plane prefab inside them + Change color script

---
+++ Mobile Project/Assets/Scripts/Utils/ChangeColor.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+[RequireComponent(typeof (MeshRenderer))]
+public class ChangeColor : MonoBehaviour
+{
+    [SerializeField]
+    private float _transitionDuration = 1.5f;
+
+    public MeshRenderer _mesh;
+    public Color _startColor = Color.white;
+
+    private Color _originalColor;
+
+    private void OnValidate()
+    {
+        _mesh = GetComponent<MeshRenderer>();
+        
+    }
+
+    private void Start()
+    {
+        _originalColor = _mesh.materials[0].GetColor("_Color");
+        ColorLerp();
+    }
+
+    private void ColorLerp()
+    {
+        _mesh.materials[0].SetColor("_Color", _startColor);
+        _mesh.materials[0].DOColor(_originalColor, _transitionDuration).SetDelay(.3f);
+    }
+}
