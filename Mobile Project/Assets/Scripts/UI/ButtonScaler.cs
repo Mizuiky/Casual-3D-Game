2bee92a85255bf23c1eaeab57cac44a403cb49d8
Priscila da Rocha Alves <priscila.mizuiky@gmail.com>
2022-08-17 17:00:02 -0300

    Add ButtonScaler to scale buttons when interacting with it

---
+++ Mobile Project/Assets/Scripts/UI/ButtonScaler.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class ButtonScaler : ClickHelper
+{
+    [Header("Animation")]
+
+    [SerializeField]
+    private float _animationDuration;
+
+    [SerializeField]
+    private float _scale;
+
+    private Tween _currentTween;
+
+    private Vector3 _originalScale;
+
+    private void Start()
+    {
+        _originalScale = transform.localScale;
+    }
+
+    protected override void Enter()
+    {
+        _currentTween = transform.DOScale(_originalScale * _scale, _animationDuration);
+    }
+
+    protected override void Exit()
+    {
+        _currentTween.Kill();
+        transform.localScale = _originalScale;
+    }
+}
