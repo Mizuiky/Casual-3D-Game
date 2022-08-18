313f1624d39e71b2b3c437cb3f6a131fc606998d
Priscila da Rocha Alves <priscila.mizuiky@gmail.com>
2022-08-18 11:38:21 -0300

    Add MainMenuHelper Script while the Arc of main menu is not complete

---
+++ Mobile Project/Assets/Scripts/Utils/MainMenuHelper.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Screen;
+using Core.Singleton;
+
+public class MainMenuHelper : MonoBehaviour
+{
+    [SerializeField]
+    private ParticleController _particleController;
+
+    [SerializeField]
+    private ScreenType _type;
+
+
+
+    private void Start()
+    {
+        UITouch.onTouchButton += GetParticleController;
+
+        ScreenManager.Instance.HideAll();
+        ScreenManager.Instance.ShowByType(_type);
+    }
+
+    private void OnDisable()
+    {
+        UITouch.onTouchButton -= GetParticleController;
+    }
+
+    public ParticleController GetParticleController()
+    {
+        if (_particleController != null)
+            return _particleController;
+
+        return null;
+    }
+
+
+}
